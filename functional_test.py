6ca0e7a64a11f84b39fe8932948d7f459fa71212
tmillenaar <timo.m@live.nl>
2018-11-28 21:09:46 +0100

    Functional test now checks the adding of a a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 # browser = webdriver.Firefox()
@@ -9,22 +11,38 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-	    self.browser = webdriver.Firefox()
-		
-	def tearDown(self):
-	    self.browser.quit()
-		
-	def test_can_start_list_and_retrieve_it_later(self):
-	    self.browser.get('http://localhost:8000')
-	    self.assertIn('My To-Do List', self.browser.title)
-	    self.fail('Finish the test!')
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    
+  def tearDown(self):
+    self.browser.quit()
+    
+  def test_can_start_list_and_retrieve_it_later(self):
+    self.browser.get('http://localhost:8000')
+    self.assertIn('My To-Do List', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('My To-Do List', header_text)
+      
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+    )
+    #Type in box to enter new todo item
+    inputbox.send_keys('Buy peacock feathers')
 
-#Type in box to enter new todo item
+    #Hit enter updates page with input
+    inputbox.send_keys(keys.ENTER)
+    time.sleep(1)
+    
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+      any(row.text == '1: Buy peacock feathers' for row in rows)
+    )
 
-#Hit enter updates page with input
-
-#Text box remains for new item
+    #Text box remains for new item
+    self.fail('Finish the test!')
 
 #Make project specific url and remember lsit
 
